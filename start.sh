d0b31759d8a8e85021fbec1852bc62432d528350
XrayHunter <51248011+XrayHunter@users.noreply.github.com>
2022-10-19 16:01:51 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo ./teamredminer -a kas -o stratum+tcp://pool.woolypooly.com:3112 -u kaspa:qz8crryt6kaa5nzclnu63lgpcu07zt3fu27va2vpvztkyl7qv9wkjgaqczn44.THX1 -p x --fpga_tmem_limit=80 --fpga_clk_core=545 --fpga_tcore_limit=70 --hardware=fpga
+sudo ./teamredminer -a kas -o stratum+tcp://pool.woolypooly.com:3112 -u kaspa:qz8crryt6kaa5nzclnu63lgpcu07zt3fu27va2vpvztkyl7qv9wkjgaqczn44.TH -p x --fpga_tmem_limit=80 --fpga_clk_core=545 --fpga_tcore_limit=70 --hardware=fpga
