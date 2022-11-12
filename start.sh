302153e31667b40e7670faaec27392b9ae53b00c
XrayHunter <51248011+XrayHunter@users.noreply.github.com>
2022-11-12 19:18:26 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo ./nextjtag -a -m  -B --set-voltage=0.60 --disable-voltage-limit  
-sudo ./teamredminer -a kas -o stratum+tcp://pool.woolypooly.com:3112 -u kaspa:qz8crryt6kaa5nzclnu63lgpcu07zt3fu27va2vpvztkyl7qv9wkjgaqczn44.TH -p x --fpga_tmem_limit=80 --fpga_clk_core=530 --fpga_tcore_limit=70 --hardware=fpga
+sudo ./teamredminer -a kas -o stratum+tcp://pool.woolypooly.com:3112 -u kaspa:qqnglxpuhp8kdn7y4p5mzgkyukdawf56ksn5clq86634rnythv3ujpj2wg8ku.TH -p x --fpga_tmem_limit=80 --fpga_clk_core=530 --fpga_tcore_limit=70 --hardware=fpga
