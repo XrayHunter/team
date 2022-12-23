1993152f8f216061697b7dc3a610df5f8b81784b
XrayHunter <51248011+XrayHunter@users.noreply.github.com>
2022-12-23 16:10:13 +0100

    Update AIR.sh

--- AIR.sh
+++ AIR.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+IP=$(hostname -I)
 sudo ./nextjtag -a -c -m
 sudo ./nextjtag -a -m  -B --set-voltage=0.64 --disable-voltage-limit --enable-allmine-vcu-bmc --disable-known-bmc-check --enable-allmine-btu9p-bmc  
-sudo ./teamredminer -a kas -o stratum+tcp://162.55.7.43:16061 -u kaspa:qqnglxpuhp8kdn7y4p5mzgkyukdawf56ksn5clq86634rnythv3ujpj2wg8ku -p x --fpga_tmem_limit=80 --fpga_clk_core=540 --fpga_tcore_limit=60 --hardware=fpga
+sudo ./teamredminer -a kas -o stratum+tcp://162.55.7.43:16061 -u kaspa:qqnglxpuhp8kdn7y4p5mzgkyukdawf56ksn5clq86634rnythv3ujpj2wg8ku -p x --fpga_tmem_limit=80 --fpga_clk_core=540 --fpga_tcore_limit=60 --hardware=fpga --api_listen=$IP:4028
