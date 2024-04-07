20f7c83a70d29a23ee07397bf17a395d3d5696a9
XrayHunter <51248011+XrayHunter@users.noreply.github.com>
2024-04-07 09:33:16 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 IP=$(hostname -I)
 #sudo ./nextjtag -a -m  -B --set-voltage=0.60 --disable-voltage-limit  
-sudo ./teamredminer -a kas -o stratum+tcp://de.alephium.herominers.com:1199 -u 18W8Nz5ZXhoDiQedakCScitnDkT844AzUdeysDX8yfDsJ.TH -p x --fpga_tmem_limit=80 --fpga_clk_core=500 --fpga_er_auto=3 --fpga_tcore_limit=65 --hardware=fpga --api_listen=$IP --fpga_vcc_int=650
+sudo ./teamredminer -a alph -o stratum+tcp://de.alephium.herominers.com:1199 -u 18W8Nz5ZXhoDiQedakCScitnDkT844AzUdeysDX8yfDsJ.TH -p x --fpga_tmem_limit=80 --fpga_clk_core=500 --fpga_er_auto=3 --fpga_tcore_limit=65 --hardware=fpga --api_listen=$IP --fpga_vcc_int=650
