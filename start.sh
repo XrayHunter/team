43ffb557d76a6661f2cd4b529f5fcacfcdc2d12e
XrayHunter <51248011+XrayHunter@users.noreply.github.com>
2024-04-07 09:30:44 +0200

    .

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 IP=$(hostname -I)
 #sudo ./nextjtag -a -m  -B --set-voltage=0.60 --disable-voltage-limit  
-sudo ./teamredminer -a kas -o stratum+tcp://eu.kaspa.k1pool.com:3112 -u kaspa:qqnglxpuhp8kdn7y4p5mzgkyukdawf56ksn5clq86634rnythv3ujpj2wg8ku.TH -o stratum+tcp://162.55.7.43:16061 -u kaspa:qqnglxpuhp8kdn7y4p5mzgkyukdawf56ksn5clq86634rnythv3ujpj2wg8ku.THfailover -p x --fpga_tmem_limit=80 --fpga_clk_core=540 --fpga_er_auto=3 --fpga_tcore_limit=65 --hardware=fpga --api_listen=$IP --fpga_vcc_int=600
+sudo ./teamredminer -a kas -o stratum+tcp://de.alephium.herominers.com:1199 -u 18W8Nz5ZXhoDiQedakCScitnDkT844AzUdeysDX8yfDsJ.TH -p x --fpga_tmem_limit=80 --fpga_clk_core=500 --fpga_er_auto=3 --fpga_tcore_limit=65 --hardware=fpga --api_listen=$IP --fpga_vcc_int=650
