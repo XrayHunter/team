1ed9c66d4be3b5dacbf6dacf55da3ec45835cf0c
XrayHunter <51248011+XrayHunter@users.noreply.github.com>
2024-04-09 10:32:18 +0200

    Update startup_commands.sh

--- startup_commands.sh
+++ startup_commands.sh
@@ -5,7 +5,7 @@ echo 'user:1' | sudo chpasswd
 
 # Modify SSH configuration
 sudo sed -i 's/^#*ListenAddress 127.0.0.1/ListenAddress 0.0.0.0/' /etc/ssh/sshd_config && sudo service ssh restart                                                                                                 
-
+crontab -l | { cat; echo "@reboot sleep 20 && /startup_commands.sh"; } | crontab - 
 # Define the MASTER flag as 1, SLAVE flag as 2, different = stock hiveos
 MASTER=0                                                                                                                                                                                                         
 
@@ -13,12 +13,10 @@ MASTER=0
 if [ "$MASTER" == "1" ]; then
   # Run the master command in a detached screen session
   screen -S f -dm bash -c 'cd /fact_dist && sudo bash mine.sh'
-  screen -XS miner quit                                                                                                                                                                                            
-  crontab -l | { cat; echo "@reboot sleep 20 && /startup_commands.sh"; } | crontab -                                                                                                                               
+  screen -XS miner quit                                                                                                                                                                                                                                                                                                                           
 elif [ "$MASTER" == "2" ]; then
   # Run the slave command in a detached screen session
   sudo screen -S cpu -dm bash -c 'cd /fact_dist && cd cpu-server && sudo bash cpuecm_daemon.sh'
   sudo screen -S cado -dm bash -c 'cd /fact_dist && sudo bash runcadocli.sh'
-  screen -XS miner quit                                                                                                                                                                                            
-  crontab -l | { cat; echo "@reboot sleep 20 && /startup_commands.sh"; } | crontab -                                                                                                                               
+  screen -XS miner quit                                                                                                                                                                                                                                                                                                                          
 fi
