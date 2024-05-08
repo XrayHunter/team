4c9554c4ff143779e255150cfa47423dc816c8c5
XrayHunter <51248011+XrayHunter@users.noreply.github.com>
2024-05-08 17:05:21 +0200

    Update startup_commands.sh

--- startup_commands.sh
+++ startup_commands.sh
@@ -2,7 +2,7 @@
 sleep 20
 # Change user password
 echo 'user:1' | sudo chpasswd                                                                                                                                                                                      
-
+cp -R /fact_dist /ramdisk/fact_dist
 # Modify SSH configuration
 sudo sed -i 's/^#*ListenAddress 127.0.0.1/ListenAddress 0.0.0.0/' /etc/ssh/sshd_config && sudo service ssh restart 
 crontab -l | { cat; echo "@reboot sleep 3 && cp -R /fact_dist /ramdisk/fact_dist"; } | crontab -
@@ -13,12 +13,11 @@ MASTER=0
 # Check if the MASTER flag is set to 1
 if [ "$MASTER" == "1" ]; then
   # Run the master command in a detached screen session
-  screen -S f -dm bash -c 'cd /fact_dist && sudo bash mine.sh'
+  screen -S f -dm bash -c 'cd /ramdisk/fact_dist && sudo bash mine.sh'
   screen -XS miner quit                                                                                                                                                                                                                                                                                                                           
 elif [ "$MASTER" == "2" ]; then
   # Run the slave command in a detached screen session
-  sudo screen -S cpu -dm bash -c 'cd /fact_dist && cd cpu-server && sudo bash cpuecm_daemon.sh'
-  sudo screen -S cado -dm bash -c 'cd /fact_dist && sudo bash runcadocli.sh'
+  screen -S slave -dm bash -c 'cd /ramdisk/fact_dist && sudo bash mine.sh'
   screen -XS miner quit                                                                                                                                                                                                                                                     
 elif [ "$MASTER" == "3" ]; then
   # Run the slave command in a detached screen session
