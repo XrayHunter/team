301aa30e44499cdf41dc92519ce1888a23634986
XrayHunter <51248011+XrayHunter@users.noreply.github.com>
2024-05-09 09:09:13 +0200

    Update startup_commands.sh

--- startup_commands.sh
+++ startup_commands.sh
@@ -3,6 +3,7 @@ sleep 20
 # Change user password
 echo 'user:1' | sudo chpasswd                                                                                                                                                                                      
 cp -R /fact_dist /ramdisk/fact_dist
+sleep 5
 # Modify SSH configuration
 sudo sed -i 's/^#*ListenAddress 127.0.0.1/ListenAddress 0.0.0.0/' /etc/ssh/sshd_config && sudo service ssh restart 
 crontab -l | { cat; echo "@reboot sleep 3 && cp -R /fact_dist /ramdisk/fact_dist"; } | crontab -
@@ -17,7 +18,8 @@ if [ "$MASTER" == "1" ]; then
   screen -XS miner quit                                                                                                                                                                                                                                                                                                                           
 elif [ "$MASTER" == "2" ]; then
   # Run the slave command in a detached screen session
-  screen -S slave -dm bash -c 'cd /ramdisk/fact_dist && sudo bash mine.sh'
+  sudo screen -S cpu -dm bash -c 'cd /fact_dist && cd cpu-server && sudo bash cpuecm_daemon.sh'
+  sudo screen -S cado -dm bash -c 'cd /fact_dist && sudo bash runcadocli.sh'
   screen -XS miner quit                                                                                                                                                                                                                                                     
 elif [ "$MASTER" == "3" ]; then
   # Run the slave command in a detached screen session
