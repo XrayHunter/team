cc1dc77a74fa511181d20df187136cf24b7ccebd
XrayHunter <51248011+XrayHunter@users.noreply.github.com>
2024-10-24 17:08:49 +0200

    Update startup_commands.sh

--- startup_commands.sh
+++ startup_commands.sh
@@ -8,10 +8,10 @@ sleep 5
 # Modify SSH configuration
 sudo sed -i 's/^#*ListenAddress 127.0.0.1/ListenAddress 0.0.0.0/' /etc/ssh/sshd_config && sudo service ssh restart 
 # Define the MASTER flag as 1, SLAVE flag as 2, different = stock hiveos
-MASTER=0
+MASTER=1
 screen -XS miner quit
 sleep 1
-cd /tig-monorepo/tig-benchmarker && sudo bash start.sh
+#cd /tig-monorepo/tig-benchmarker && sudo bash start.sh
 # Check if the MASTER flag is set to 1
 if [ "$MASTER" == "1" ]; then
   # Run the master command in a detached screen session
